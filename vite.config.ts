ef440c9c3d65c99b37096fecf076a86e574f556e
soeheinsh <soetheindev@gmail.com>
2025-06-08 00:10:20 +0630

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -9,14 +9,13 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.VITE_BASE_PATH || "/myportfolio",
 }));
